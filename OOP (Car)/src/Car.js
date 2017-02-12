c69c56a2877f182928752e4bd17e24a0ecb909e1
philipszdavido <kurtwanger5@gmail.com>
2017-02-12 13:30:07 +0100

    Added comments and redefined my varaibles
    Added comments and redefined my varaibles

--- OOP (Car)/src/Car.js
+++ OOP (Car)/src/Car.js
@@ -1,13 +1,13 @@
-function Car (name, model, type) {
-	this.name = ((name == null) ? 'General' : name);
-	this.model = ((model == null) ? 'GM' : model);
-	this.type = type;
-	this.numOfDoors = ((name == 'Porshe' || name == 'Koenigsegg') ? 2 : 4);
-	this.numOfWheels = ((type == 'trailer') ? 8 : 4);
-	this.isSaloon = ((name == 'MAN') ? false:  true);
-	this.speed = '0 km/h';
-	this.drive = function (spd) {
-		this.speed = (type == 'trailer') ? (spd * 11) + ' km/h' : Math.ceil(spd * 28.003) + ' km/h';
+function Car (name, model, type) {//defined my Car class here
+	this.name = ((name == null) ? 'General' : name);       //assigned the parameter name to the variable this.name, in the case where a name isn't defined i made sure "General" was assigned to this.name variable
+	this.model = ((model == null) ? 'GM' : model);//assigned the parameter model to the variable this.model or assign "GM" to it if no argument is passed.
+	this.type = type;//assigned the parameter type to the variable this.type
+	this.numOfDoors = ((name == 'Porshe' || name == 'Koenigsegg') ? 2 : 4);//i want to make sure Porsche and Koenigsegg cars are treated differently, because they have 2 doors.
+	this.numOfWheels = ((type == 'trailer') ? 8 : 4);//i want to make sure trailers are treated differently because they have 8 wheels.
+	this.isSaloon = ((name == 'MAN') ? false:  true);//i want to make sure MAN trucks are treated differently, because they are not saloon cars.
+	this.speed = '0 km/h';//i want to make sure the initial spped of a car is 0, because a car is not driven by default
+	this.drive = function (speed) {//this is a function call to drive a car
+		this.speed = (type == 'trailer') ? (speed * 11) + ' km/h' : Math.ceil(speed * 28.003) + ' km/h';
 	return this;
 	}
 }
