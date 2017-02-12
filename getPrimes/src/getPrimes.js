f2cb08841a0be96f9487a551b01aeaa9aaa30a1c
philipszdavido <kurtwanger5@gmail.com>
2017-02-12 13:18:31 +0100

    Edited the getPrimes function
    Edited the getPrimes function to fit into my testcases

--- getPrimes/src/getPrimes.js
+++ getPrimes/src/getPrimes.js
@@ -1,12 +1,12 @@
 'use strict'
  
- function getPrimes(num) {
+ function getPrimes(number) {
   var primes = [2,3];
 
-  var checkPrime = function(num) {
+  var checkPrime = function(number) {//this is a function call to check for prime number.
     var numIsPrime;
-    for(var i = 2; i < num; i++){
-           if(num % i === 0){
+    for(var i = 2; i < number; i++){
+           if(number % i === 0){
                numIsPrime = false ;
                break;
            }
@@ -17,20 +17,24 @@
       return numIsPrime;
   }
 
-  if (num < 0) {
+  //checks if the argument number is a negative number.
+  if (number < 0) {
     return "negative integers can not be prime";
   }
-  else if (num === 1) {
+  //check if the argument number is equals to 1, because 1 is not a prime number
+  else if (number === 1) {
     return '1 is not a prime number';  
   }
-  else if (num === 2) {
+  //checks if the argument number is equals to 2.
+  else if (number === 2) {
     return [2];
   }
-  else if (num === 3) {
+  //checks if the argument number is equals to 3.
+  else if (number === 3) {
     return primes;
   }
   else {
-    for(var i = 4; i <= num; i++) {
+    for(var i = 4; i <= number; i++) {
           if(checkPrime(i) === true ){
               primes.push(i);
           }
@@ -38,4 +42,3 @@
     return primes;
   }
 }
-
